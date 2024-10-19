42672e801098e1020b84df73c24e1f4a0d6d92c0
Vishal Parmar <160700259+vishaljinode@users.noreply.github.com>
2024-10-19 16:54:54 +0530

    Update userTokenController.js

--- src/controllers/userTokenController.js
+++ src/controllers/userTokenController.js
@@ -3,16 +3,20 @@ const UserToken = userTokenModel.userToken;
 
 const createToken = async (req, res) => {
   try {
+
+    console.log(" req.body", req.body)
     const device_id = req.body.device_id;
     const fcmtoken = req.body.fcmtoken;
 
     if(!device_id){
+      console.log("Device ID is required")
       return res.status(400).json({message: "Device ID is required"})
     }
     if(!fcmtoken){
+      console.log("FCM Token is required")
       return res.status(400).json({message: "FCM Token is required"})
       }
-      
+
 
     // Use findOneAndUpdate with upsert option
     const userToken = await UserToken.findOneAndUpdate(
@@ -22,12 +26,14 @@ const createToken = async (req, res) => {
     );
 
     if (userToken.isNew) {
+      console.log("Token created successfully")
       return res.json({ message: "Token created successfully" });
     } else {
+      console.log("Token updated successfully")
       return res.json({ message: "Token updated successfully" });
     }
   } catch (err) {
-    console.error(err.message);
+    console.error("Token error",err.message);
     return res.status(500).json({ message: "An error occurred" });
   }
 };
