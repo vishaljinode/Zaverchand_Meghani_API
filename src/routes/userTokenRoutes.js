21e4b633bf5b71fcbb3fd4b3e7035b6183566b65
Vishal Parmar <160700259+vishaljinode@users.noreply.github.com>
2024-12-18 11:54:24 +0530

    add firebase

--- src/routes/userTokenRoutes.js
+++ src/routes/userTokenRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userTokenRouter = express.Router();
 
-const { createToken } = require("../controllers/userTokenController");
+const { createToken, sendNotification } = require("../controllers/userTokenController");
 
 userTokenRouter.post("/createUserToken", createToken);
+userTokenRouter.post("/sendnotification", sendNotification);
 
 module.exports = userTokenRouter;
