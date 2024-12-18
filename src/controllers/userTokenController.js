47b15662d815c73e8a9c9ca6e6f2d6d1891d525a
Vishal Parmar <160700259+vishaljinode@users.noreply.github.com>
2024-12-18 11:54:00 +0530

    add firebase

--- src/controllers/userTokenController.js
+++ src/controllers/userTokenController.js
@@ -1,5 +1,7 @@
 const userTokenModel = require("../models/userTokenSchema");
 const UserToken = userTokenModel.userToken;
+const { firebaseAdmin } = require('../config/firebaseConfig');
+
 
 const createToken = async (req, res) => {
   try {
@@ -38,4 +40,65 @@ const createToken = async (req, res) => {
   }
 };
 
-module.exports = { createToken };
+const sendNotification = async (req, res) => {
+  const { title, description } = req.body;
+  
+  // Check if title and description are provided
+  if (!title) {
+    return res.status(400).json({ message: "Title is required" });
+  }
+
+  if (!description) {
+    return res.status(400).json({ message: "Description is required" });
+  }
+
+  const messageBase = {
+    webpush: {
+      notification: {
+        title: title,
+        body: description,
+        // icon: iconUrl, // Use the resolved iconUrl if needed
+      },
+    },
+  };
+
+  try {
+    // Get user tokens from the database
+    const userTokens = await UserToken.find({});
+    
+    if (userTokens.length === 0) {
+      console.error("No FCM tokens found.");
+      return res.status(404).json({ message: "No users to notify." });
+    }
+
+    // Send notifications to each user
+    const notificationPromises = userTokens.map(async (item) => {
+      if (!item.fcm_token) {
+        console.warn("Empty FCM token for user:", item.id);
+        return;
+      }
+
+      const payload = { ...messageBase, token: item.fcm_token };
+
+      try {
+        // Send the notification via Firebase Cloud Messaging
+        const response = await firebaseAdmin.messaging().send(payload);
+        console.log("Notification sent successfully:", response);
+      } catch (error) {
+        console.error("Error sending FCM message:", error);
+      }
+    });
+
+    // Wait for all notifications to be sent
+    await Promise.all(notificationPromises);
+
+    return res.status(200).json({ message: "Notifications sent successfully." });
+  } catch (error) {
+    console.error("Error in sending notifications:", error);
+    return res.status(500).json({ message: "Error in sending notifications." });
+  }
+};
+
+
+
+module.exports = { createToken, sendNotification };
